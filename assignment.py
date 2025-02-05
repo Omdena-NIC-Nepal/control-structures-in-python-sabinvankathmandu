90bf89c473341028a010265af281d56bcad760f1
sabinvankathmandu <sabin.bajracharya@hotmail.com>
2025-02-05 22:24:56 +0100

    task 1

--- assignment.py
+++ assignment.py
@@ -1,1 +1,8 @@
-asdfasfd a s
+def while_loop():
+    number = 0
+    while number <= 20:
+        if number % 2 == 0:
+            print(number)
+        if number == 16:
+            break
+        number += 1
